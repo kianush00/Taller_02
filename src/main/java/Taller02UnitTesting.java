7d5e8336e6e21e6eaae061dfc34192316037f58f
Kianush Atighi-Moghaddam <64434787+kianush00@users.noreply.github.com>
2020-12-01 10:32:28 -0300

    "se instancian los métodos para registrar intensidad, día y hora del mayor sismo, y se implementan tests para cada método. Ahora los métodos propagan errores en caso de que el arreglo sea vacío o tenga tamaño vació

--- src/main/java/Taller02UnitTesting.java
+++ src/main/java/Taller02UnitTesting.java
@@ -5,24 +5,70 @@ public class Taller02UnitTesting {
 
     public void imprimirDatosSismoMasIntenso(double[][] sensor){
         System.out.println("Datos del sismo más intenso: ");
-        System.out.println("Intensidad: ");
-        System.out.println("Día en que ocurrió: ");
-        System.out.println("Hora en que ocurrió: ");
+        System.out.println("Intensidad: " + registrarIntensidadMayorSismo(sensor));
+        System.out.println("Día en que ocurrió: " + registrarDiaMayorSismo(sensor));
+        System.out.println("Hora en que ocurrió: " + registrarHoraMayorSismo(sensor));
     }
 
     public double registrarIntensidadMayorSismo(double[][] sensor){
+        propagarExcepcion(sensor);
+
         double mayor = sensor[0][0];
+        double subMayor;
 
         for (int i = 0; i < sensor.length; i++){
-            mayor = sensor[i][0];
+            subMayor = sensor[i][0];
             for (int j = 1; j < 24; j++){
-                if(sensor[i][j] > mayor){
-
+                if(sensor[i][j] > subMayor){
+                    subMayor = sensor[i][j];
                 }
+            }
+            if (subMayor > mayor){
+                mayor = subMayor;
             }
         }
 
         return mayor;
     }
 
+    public double registrarDiaMayorSismo(double[][] sensor) throws IllegalArgumentException{
+        propagarExcepcion(sensor);
+
+        double mayorSismo = registrarIntensidadMayorSismo(sensor);
+
+        for (int i = 0; i < sensor.length; i++){
+            for (int j = 0; j < 24; j++){
+                if(sensor[i][j] == mayorSismo){
+                    return i;
+                }
+            }
+        }
+
+        return 0;
+    }
+
+    public double registrarHoraMayorSismo(double[][] sensor) throws IllegalArgumentException{
+        propagarExcepcion(sensor);
+
+        double mayorSismo = registrarIntensidadMayorSismo(sensor);
+
+        for (int i = 0; i < sensor.length; i++){
+            for (int j = 0; j < 24; j++){
+                if(sensor[i][j] == mayorSismo){
+                    return j;
+                }
+            }
+        }
+
+        return 0;
+    }
+
+    private void propagarExcepcion(double[][] sensor) throws IllegalArgumentException{
+        if(sensor == null){
+            throw new IllegalArgumentException("el sensor es vacío");
+        }
+        if(sensor.length == 0 || sensor[0].length == 0){
+            throw new IllegalArgumentException("el sensor contiene un tamaño vacío");
+        }
+    }
 }
--- src/test/java/Taller02UnitTestingTest.java
+++ src/test/java/Taller02UnitTestingTest.java
@@ -1,15 +1,60 @@
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class Taller02UnitTestingTest {
+    private Taller02UnitTesting t;
+    private double[][] sensor;
+    private double[][] esperado;
 
     @Before
     public void setUp() throws Exception {
+        t = new Taller02UnitTesting();
     }
 
     @After
     public void tearDown() throws Exception {
+        t = null;
+    }
+
+    @Test
+    public void testregistrarIntensidadMayorSismo01(){
+        sensor = new double[2][24];
+        sensor[0][0] = 3;
+        sensor[0][1] = 5;
+        sensor[0][2] = 6.5;
+        sensor[0][3] = 4.5;
+        sensor[1][0] = 3;
+        sensor[1][1] = 7;
+        sensor[1][2] = 4;
+        assertEquals(7,t.registrarIntensidadMayorSismo(sensor),0.1);
+    }
+
+    @Test
+    public void testregistrarDiaMayorSismo01(){
+        sensor = new double[2][24];
+        sensor[0][0] = 3;
+        sensor[0][1] = 5;
+        sensor[0][2] = 6.5;
+        sensor[0][3] = 4.5;
+        sensor[1][0] = 3;
+        sensor[1][1] = 7;
+        sensor[1][2] = 4;
+        assertEquals(1,t.registrarDiaMayorSismo(sensor),0.1);
+    }
+
+    @Test
+    public void testRegistrarHoraMayorSismo(){
+        sensor = new double[2][24];
+        sensor[0][0] = 3;
+        sensor[0][1] = 5;
+        sensor[0][2] = 6.5;
+        sensor[0][3] = 4.5;
+        sensor[1][0] = 3;
+        sensor[1][1] = 4;
+        sensor[1][2] = 7;
+        assertEquals(2,t.registrarHoraMayorSismo(sensor),0.1);
     }
 }
