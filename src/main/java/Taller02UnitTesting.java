0b3b1db35919b735a5a4ef0c557ddb16251efcc3
Arturo <a.avendano04@ufromail.cl>
2020-12-01 09:35:53 -0300

    Inicio funcion de alertaSismica.

--- src/main/java/Taller02UnitTesting.java
+++ src/main/java/Taller02UnitTesting.java
@@ -1,3 +1,26 @@
+import java.util.Scanner;
+
 public class Taller02UnitTesting {
-    
+
+    /*public int setUpsensor(){
+        System.out.println("Ingrese cantidad de dias a estudiar");
+        Scanner scanner = new Scanner(System.in);
+        int entrada;
+        try {
+                entrada = scanner.nextInt();
+            if (entrada < 0) {
+                throw new IllegalArgumentException();
+            }
+
+        }
+        catch(IllegalArgumentException iae){
+
+            }
+
+        }*/
+
+    public int alertaSismica(double[][] entrada )
+    {        return 0;
 }
+}
+
