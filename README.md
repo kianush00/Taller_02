e87e5d9c2af4d744ac42960fe0288f5ff2ea9f01
Kianush Atighi-Moghaddam <64434787+kianush00@users.noreply.github.com>
2020-12-01 10:03:57 -0300

    "se instancia el método "registrarIntensidadMayorSismo"

---
+++ README.md
@@ -0,0 +1,6 @@
+Integrantes:
+ Kianush Atighi-Moghaddam
+ -Hará las partes 1 y 2 de la información a ser entregada
+
+ Arturo Avendaño
+ -Hará las partes 3 y 4 de la información a ser entregada
--- src/main/java/Taller02UnitTesting.java
+++ src/main/java/Taller02UnitTesting.java
@@ -3,8 +3,26 @@ public class Taller02UnitTesting {
     // double[][] sensor = new double[Ndias][24];
     //  reciclar metodo random para los 24 dias (0.0 a 9.5) (10 a 95 luego / por 10)
 
-    public void registrarSismoMasIntenso(double[][] sensor){
+    public void imprimirDatosSismoMasIntenso(double[][] sensor){
+        System.out.println("Datos del sismo más intenso: ");
+        System.out.println("Intensidad: ");
+        System.out.println("Día en que ocurrió: ");
+        System.out.println("Hora en que ocurrió: ");
+    }
 
+    public double registrarIntensidadMayorSismo(double[][] sensor){
+        double mayor = sensor[0][0];
+
+        for (int i = 0; i < sensor.length; i++){
+            mayor = sensor[i][0];
+            for (int j = 1; j < 24; j++){
+                if(sensor[i][j] > mayor){
+
+                }
+            }
+        }
+
+        return mayor;
     }
 
 }
