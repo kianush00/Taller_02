ef0cba54de929b8f4c1bdd47b10252258db39e23
Arturo <a.avendano04@ufromail.cl>
2020-12-01 10:33:37 -0300

    Finalizacion de setupSensor, avance de menu()

--- src/main/java/Taller02UnitTesting.java
+++ src/main/java/Taller02UnitTesting.java
@@ -2,29 +2,41 @@ import java.util.Scanner;
 
 public class Taller02UnitTesting {
 
-    public int setUpsensor(){
+    public double[][] setUpsensor(){
         System.out.println("Ingrese cantidad de dias a estudiar");
         Scanner scanner = new Scanner(System.in);
-        int entrada;
-        try {
-                entrada = scanner.nextInt();
-            if (entrada < 0) {
-                throw new IllegalArgumentException();
+        int entrada=0;
+        boolean intento=false;
+       while(intento) {
+           try {
+               entrada = scanner.nextInt();
+               if (entrada < 0) {
+                   throw new IllegalArgumentException();
+               }
+               else{
+                   intento=true;
+               }
+           } catch (IllegalArgumentException iae) {
+               System.err.println("Error detectado, argumento invalido, ingrese un numero.");
+
+           }
+
+       }
+            double[][] sensor = new double[entrada][24];
+            for(int i=0; i>sensor.length;++i){
+                for(int j=0;j>24;++i){
+                    sensor[i][j]=getValorSismicoAleatorioEntre(0.0,9.5);
+                }
             }
-
-        }
-        catch(IllegalArgumentException iae){
-            System.err.println("Error detectado, argumento invalido, ingrese un numero.");
-            }
-            return 0;
+            return sensor;
         }
 
 
-    public int getEnteroAleatorioEntre(int min, int max){
+    private double getValorSismicoAleatorioEntre(double min, double max){
         if(min > max){   //si los argumentos tienen valores contrarios, entonces se cambiarán de posición
-            return (int) (Math.random() * ((min + 1) - max)) + max;
+            return (Math.random() * ((min + 1) - max)) + max;
         }
-        return (int) (Math.random() * ((max + 1) - min)) + min;
+        return (Math.random() * ((max + 1) - min)) + min;
     }
 
     public int alertaSismica(double[][] sensor ) {
@@ -45,6 +57,13 @@ public class Taller02UnitTesting {
         return 0;
     }
     public int menu(){
+        double sensor[][]=setUpsensor();
+        alertaSismica(sensor);
+        //imprimirDatoMayorSismo();
+
+        //registrarIntensidadmayorSismo
+        //registrarHoraMayorSismo
+
         return 0;
     }
 
--- src/test/java/Taller02UnitTestingTest.java
+++ src/test/java/Taller02UnitTestingTest.java
@@ -4,12 +4,16 @@ import org.junit.Before;
 import static org.junit.Assert.*;
 
 public class Taller02UnitTestingTest {
+    private Taller02UnitTesting t;
+    private double[][] sensor;
 
     @Before
     public void setUp() throws Exception {
+        t = new Taller02UnitTesting();
     }
 
     @After
     public void tearDown() throws Exception {
+        t = null;
     }
 }
