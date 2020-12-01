a5a3414424a1cb5a7dd76d57a005c4a692da5e1b
Kianush Atighi-Moghaddam <64434787+kianush00@users.noreply.github.com>
2020-12-01 09:31:19 -0300

    "se instancia un método para registrar el sismo más intenso"

--- src/main/java/Taller02UnitTesting.java
+++ src/main/java/Taller02UnitTesting.java
@@ -1,3 +1,10 @@
 public class Taller02UnitTesting {
-    
+    //   N = intro.nextInt(); (validar)
+    // double[][] sensor = new double[Ndias][24];
+    //  reciclar metodo random para los 24 dias (0.0 a 9.5) (10 a 95 luego / por 10)
+
+    public void registrarSismoMasIntenso(double[][] sensor){
+
+    }
+
 }
